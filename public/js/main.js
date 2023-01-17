f5264669f73f94de576c5d5a890e1925f05853e5
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-17 09:36:00 +0100

    Evenement double clic

--- public/js/main.js
+++ public/js/main.js
@@ -5,5 +5,9 @@
 
 // ### 1. Au double clique du h1, son fond devient rouge et son texte devient blanc
 let h1 = document.querySelector('h1');
+h1.addEventListener('dblclick', () => {
+    h1.style.backgroundColor = 'red';
+    h1.style.color = 'white';
+})
 // ### 2. Au hover du h3, son texte rétrécit d'un caractère 
 // ### 3. Quand on clique sur le paragraphe le fond devient bleu et le texte gold, mais si le fond et déjà bleu et le texte déjà gold alors le fond redevient blanc et le texte noir
